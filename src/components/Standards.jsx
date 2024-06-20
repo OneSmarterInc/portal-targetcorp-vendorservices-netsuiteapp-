e30e57850f84229e691d4934d201af349d0bc91b
nachiket01 <nachikettekade01@gmail.com>
2024-06-20 15:47:41 +0530

    done

--- src/components/Standards.jsx
+++ src/components/Standards.jsx
@@ -9,16 +9,16 @@ const Standards = () => {
         We hold ourselves and our vendors to the highest standards. Please review the following and ensure you can commit to:
       </p>
       <ul>
-        <li>Target’s <a href="https://www.partnersonline.com/page/public/legal/Rules%20of%20Use">Rules of Use</a></li>
-        <li>Target’s <a href="https://corporate.target.com/sustainability-governance/responsible-supply-chains/audit-program">audit program</a> and <a href="https://corporate.target.com/sustainability-governance/responsible-supply-chains/suppliers/standards-of-vendor-engagement">Standards of Vendor Engagement</a></li>
+        <li>Target’s <a href="https://www.partnersonline.com/page/public/legal/Rules%20of%20Use" target='blank_'>Rules of Use</a></li>
+        <li>Target’s <a href="https://corporate.target.com/sustainability-governance/responsible-supply-chains/audit-program" target='blank_'>audit program</a> and <a href="https://corporate.target.com/sustainability-governance/responsible-supply-chains/suppliers/standards-of-vendor-engagement" target='blank_'>Standards of Vendor Engagement</a></li>
         
-        <li>Target’s <a href="https://corporate.target.com/sustainability-governance/operating-ethically/product-safety-quality-assurance">Product Safety & Quality Assurance approach</a></li>
-        <li>Target’s <a href="https://corporate.target.com/sustainability-governance/responsible-supply-chains/suppliers/book-content-guidelines">Book Content Guidelines</a></li>
-        <li>Target’s <a href="https://corporate.target.com/sustainability-ESG/governance-and-reporting/Business-Partner-Code-of-Conduct">Business Partner Code of Conduct</a></li>
+        <li>Target’s <a href="https://corporate.target.com/sustainability-governance/operating-ethically/product-safety-quality-assurance" target='blank_'>Product Safety & Quality Assurance approach</a></li>
+        <li>Target’s <a href="https://corporate.target.com/sustainability-governance/responsible-supply-chains/suppliers/book-content-guidelines" target='blank_'>Book Content Guidelines</a></li>
+        <li>Target’s <a href="https://corporate.target.com/sustainability-ESG/governance-and-reporting/Business-Partner-Code-of-Conduct" target='blank_'>Business Partner Code of Conduct</a></li>
         <li>Use of Electronic Funds Transfer (EFT) with our Accounts Payable team, required for domestic-based vendors</li>
         <li>Ability to use EDI to receive purchase orders</li>
         <li>Providing certificate of insurance</li>
-        <li>Eligibility considerations for <a href="https://corporate.target.com/getmedia/9ed69b6d-8908-4638-b255-1f5476328440/Target-Drop-Ship-Eligibility.pdf">drop shipping</a></li>
+        <li>Eligibility considerations for <a href="https://corporate.target.com/getmedia/9ed69b6d-8908-4638-b255-1f5476328440/Target-Drop-Ship-Eligibility.pdf" target='blank_'>drop shipping</a></li>
       </ul>
     </div>
   );
